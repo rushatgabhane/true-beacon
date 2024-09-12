875d3dfd536facd429cc47d3443dcaa288b89972
Rushat Gabhane <rushatgabhane@gmail.com>
2024-09-12 21:35:44 +0300

    enable cors on backend

--- go.mod
+++ go.mod
@@ -9,3 +9,5 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.23
 	golang.org/x/crypto v0.27.0
 )
+
+require github.com/rs/cors v1.11.1 // indirect
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -23,6 +24,7 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(cors.Default().Handler)
 
 	// Public Routes
 	r.Group(func(r chi.Router) {
