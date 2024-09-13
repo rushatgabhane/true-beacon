ecad70f08dc05a4ec5630249275871f34386c775
Rushat Gabhane <rushatgabhane@gmail.com>
2024-09-13 04:37:13 +0300

    display total pnl

--- frontend/src/components/holdingTable.tsx
+++ frontend/src/components/holdingTable.tsx
@@ -2,16 +2,14 @@ import { useEffect, useState } from 'react';
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from './ui/table';
 import { getHoldings } from '@/lib/actions/holding';
 import { toDateString } from '@/lib/utils';
-import { Card, CardHeader, CardTitle } from './ui/card';
+import { Card, CardFooter, CardHeader, CardTitle } from './ui/card';
 
 type Holding = {
   authorised_date: string;
@@ -30,6 +28,8 @@ type Holding = {
 export default function HoldingTable() {
   const [holdings, setHoldings] = useState<Holding[]>([]);
 
+  const totalPnl = holdings.reduce((acc, holding) => acc + holding.pnl, 0);
+
   useEffect(() => {
     getHoldings().then((holdings) => {
       setHoldings(holdings.data);
@@ -42,44 +42,53 @@ export default function HoldingTable() {
         <CardHeader>
           <CardTitle>Holdings</CardTitle>
         </CardHeader>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Authorised date</TableHead>
-              <TableHead>Average price</TableHead>
-              <TableHead>Close price</TableHead>
-              <TableHead>Day change</TableHead>
-              <TableHead>Day change %</TableHead>
-              <TableHead>Exchange</TableHead>
-              <TableHead>ISIN</TableHead>
-              <TableHead>Last price</TableHead>
-              <TableHead>PNL</TableHead>
-              <TableHead>Quantity</TableHead>
-              <TableHead>Trading Symbol</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {holdings.map((holding) => (
-              <TableRow key={holding.isin}>
-                <TableCell className="font-medium">
-                  {toDateString(new Date(holding.authorised_date))}
-                </TableCell>
-                <TableCell>{holding.average_price.toFixed(2)}</TableCell>
-                <TableCell>{holding.close_price.toFixed(2)}</TableCell>
-                <TableCell>{holding.day_change.toFixed(2)}</TableCell>
-                <TableCell>
-                  {holding.day_change_percentage.toFixed(2)}
-                </TableCell>
-                <TableCell>{holding.exchange}</TableCell>
-                <TableCell>{holding.isin}</TableCell>
-                <TableCell>{holding.last_price}</TableCell>
-                <TableCell>{holding.pnl.toFixed(2)}</TableCell>
-                <TableCell>{holding.quantity}</TableCell>
-                <TableCell>{holding.tradingsymbol}</TableCell>
+        <div className="px-4">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Authorised date</TableHead>
+                <TableHead>Average price</TableHead>
+                <TableHead>Close price</TableHead>
+                <TableHead>Day change</TableHead>
+                <TableHead>Day change %</TableHead>
+                <TableHead>Exchange</TableHead>
+                <TableHead>ISIN</TableHead>
+                <TableHead>Last price</TableHead>
+                <TableHead>PNL</TableHead>
+                <TableHead>Quantity</TableHead>
+                <TableHead>Trading Symbol</TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHeader>
+            <TableBody>
+              {holdings.map((holding) => (
+                <TableRow key={holding.isin}>
+                  <TableCell className="font-medium">
+                    {toDateString(new Date(holding.authorised_date))}
+                  </TableCell>
+                  <TableCell>{holding.average_price.toFixed(2)}</TableCell>
+                  <TableCell>{holding.close_price.toFixed(2)}</TableCell>
+                  <TableCell>{holding.day_change.toFixed(2)}</TableCell>
+                  <TableCell>
+                    {holding.day_change_percentage.toFixed(2)}
+                  </TableCell>
+                  <TableCell>{holding.exchange}</TableCell>
+                  <TableCell>{holding.isin}</TableCell>
+                  <TableCell>{holding.last_price}</TableCell>
+                  <TableCell>{holding.pnl.toFixed(2)}</TableCell>
+                  <TableCell>{holding.quantity}</TableCell>
+                  <TableCell>{holding.tradingsymbol}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+        <CardFooter>
+          <div className="mt-4">
+            {!isNaN(totalPnl) && (
+              <CardTitle>Total PNL: {totalPnl.toFixed(2)}</CardTitle>
+            )}
+          </div>
+        </CardFooter>
       </Card>
     )
   );
